37d038b43debc91dfdfc0c7ef3abc6c1407bb784
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-04-18 15:30:14 +0300

    checkType was fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,30 @@ import { checkType } from './checkType';
 
 import './App.scss';
 
+const output = Object.entries(input).reduce((obj, [key, value]) => {
+  return {
+    ...obj,
+    [key]: checkType(value),
+  }
+}, {});
+
 function App() {
   const data = JSON.parse(JSON.stringify(input));
 
+  const outputData = (value) => {
+    if (typeof value === 'boolean') {
+      return JSON.stringify(value);
+    }
+    if (typeof value === 'object') {
+      return JSON.stringify(value);
+    } else {
+      return value;
+    }
+  };
+
   return (
     <div className="app">
-      <h1>Web app to convert one json to another json file</h1>
+      <h1 className="app__main-title">Web app to convert one json to another json file</h1>
       <div className="app__container">
         <div className="app__output">
           <div className="app__title-container">
@@ -19,7 +37,7 @@ function App() {
             return (
               <div key={key} className="app__list-container">
                 <p className="app__list-key">{`${key}:`}</p>
-                <p className="app__list-value">{`${value}`}</p>
+                <p className="app__list-value">{outputData(value)}</p>
               </div>
             )
           })}
@@ -29,11 +47,11 @@ function App() {
             <p className="app__title">key</p>
             <p className="app__title-value">converted value</p>
           </div>
-          {Object.entries(data).map(([key, value]) => {
+          {Object.entries(output).map(([key, value]) => {
             return (
               <div key={key} className="app__list-container">
                 <p className="app__list-key">{`${key}:`}</p>
-                <p className="app__list-value">{`${checkType(value)}`}</p>
+                <p className="app__list-value">{value}</p>
               </div>
             )
           })}
--- src/checkType.js
+++ src/checkType.js
@@ -1,54 +1,131 @@
-// import output from './api/output.json'
+const isObject = (value) => typeof value === 'object';
+
+const isBoolean = (value) => typeof value === 'boolean';
+
+const isEmail = (value) => {
+  const emailInput = document.createElement('input');
+  emailInput.type = 'email';
+  emailInput.value = value;
+  return emailInput.checkValidity();
+};
+
+const isURL = (value) => {
+  const urlInput = document.createElement('input');
+  urlInput.type = 'url';
+  urlInput.value = value;
+  return urlInput.checkValidity();
+};
+
+const isTel = (value) => value.split('').every(char => '+ 0123456789'.includes(char));
+
+const isPostalCode = (value) => value.substr(0, 2) === '65'
+  && value.length === 5 && value.split('').every(char => '0123456789'.includes(char));
+
+const isInteger = (value) => value.split('').every(char => '0123456789'.includes(char));
+
+const isUUID = (value) => {
+  if (value.split('').every(char => '0123456789abcdefghijklmnopqrstuvwxyz-ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(char))) {
+    const partOfisUUID = value.split('-');
+    if (partOfisUUID[0].length === 8 && partOfisUUID[1].length === 4
+      && partOfisUUID[2].length === 4 && partOfisUUID[3].length === 4
+      && partOfisUUID[4].length === 12 && value.length === 36) {
+      return true;
+    }
+  }
+}
+
+const isDate = (value) => {
+  if ((value.split('.').length - 1 === 2
+    || value.split('/').length - 1 === 2
+    || value.split('-').length - 1 === 2)
+    && value.split('').every(char => '0123456789./-'.includes(char))) {
+    const partOfDateWithDot = value.split('.');
+    const partOfDateWithSlesh = value.split('/');
+    const partOfDateWithDash = value.split('-');
+
+    if (partOfDateWithDot[0] >= 1 && partOfDateWithDot[0] <= 31
+      && partOfDateWithDot[1] >= 1 && partOfDateWithDot[1] <= 12) {
+      return true;
+    } if (partOfDateWithSlesh[0] >= 1 && partOfDateWithSlesh[0] <= 31
+      && partOfDateWithSlesh[1] >= 1 && partOfDateWithSlesh[1] <= 12) {
+      return true;
+    } if (partOfDateWithDash[0] >= 1 && partOfDateWithDash[0] <= 31
+      && partOfDateWithDash[1] >= 1 && partOfDateWithDash[1] <= 12) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+const isIP = (value) => {
+  if (value.split('.').length - 1 === 3
+    && value.split('').every(char => '0123456789.'.includes(char))) {
+    const partOfIP = value.split('.');
+    if (partOfIP[0] >= 0 && partOfIP[0] <= 250
+      && partOfIP[1] >= 0 && partOfIP[1] <= 250
+      && partOfIP[2] >= 0 && partOfIP[2] <= 250
+      && partOfIP[3] >= 0 && partOfIP[3] <= 250) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+const isFloat = (value) => {
+  const partOfValue = value.split('.');
+  return partOfValue.length - 1 === 1 && partOfValue[0] !== '' && partOfValue[1] !== ''
+    && value.split('').every(char => '.0123456789'.includes(char))
+};
+
+const isAddress = (value) => value.includes('street' || 'square' || 'avenue');
+
+const isLongText = (value) => {
+  return /\w+\s+\w+\s+\w+\w+\s/.test(value);
+};
+
+const isShortText = (value) => {
+  return /\w+\s+\w+\s+\w+/.test(value);
+};
+
+const isWord = (value) => value.split('')
+  .every(char => 'abcdefghijklmnopqrstuvwxyz'.includes(char));
 
 export const checkType = (value) => {
   if (Array.isArray(value)) {
     return 'array';
-  }
-  if (typeof value === 'object') {
+  } else if (isObject(value)) {
     return 'object';
-  }
-  if (/true|false/.test(value)) {
+  } else if (isBoolean(value)) {
     return 'boolean';
-  }
-  if (/^\d{5}$/.test(value)) {
+  } else if (isPostalCode(value)) {
     return 'postal code'
-  }
-  if (/^\d+$/.test(value)) {
+  } else if (isInteger(value)) {
     return 'integer number'
-  }
-  if (/^[{(]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[)}]?$/.test(value)) {
+  } else if (isUUID(value)) {
     return 'universal unique identifier';
-  }
-  if (/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/.test(value)) {
+  } else if (isTel(value)) {
     return 'phone number';
-  }
-  if (/(0?[1-9]|[12][0-9]|3[01])[\/\-\.](0?[1-9]|1[012])[ \/\.\-]/.test(value)) {
+  } else if (isDate(value)) {
     return 'date or datetime';
-  }
-  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\:([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]))?/.test(value)) {
+  } else if (isIP(value)) {
     return 'internet protocol address';
-  }
-  if (/^[0-9]+\.[0-9]+$/.test(value)) {
+  } else if (isFloat(value)) {
     return 'floating - point digit';
-  }
-  if (/^(http|https|ftp):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)/i.test(value)) {
+  } else if (isURL(value)) {
     return 'url';
-  }
-  if (/\w+\@\w+\.\w+/.test(value)) {
-    return 'email'
-  }
-  if (/\[a-z]\street|str/.test(value)) {
+  } else if (isEmail(value)) {
+    return 'email';
+  } else if (isAddress(value)) {
     return 'street address';
-  }
-  if (/\w+\s+\w+\s+\w+\w+\s/.test(value)) {
+  } else if (isLongText(value)) {
     return 'long text';
-  }
-  if (/\w+\s+\w+\s+\w+/.test(value)) {
+  } else if (isShortText(value)) {
     return 'short text';
-  }
-  if (/^\w+$/.test(value)) {
+  } else if (isWord) {
     return 'word';
+  } else {
+    return 'undefined';
   }
-
-  return 'underfind'
 };
